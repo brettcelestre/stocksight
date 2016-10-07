12df22e079db62ba7d1a768a50935a03a3de2e16
Brett Celestre <bcelestre@gmail.com>
2016-10-06 22:03:00 -0700

    Adds basic chart labels

--- client/app/shared/chart/chartController.js
+++ client/app/shared/chart/chartController.js
@@ -3,19 +3,21 @@ angular.module('StockSight.main.chart', [])
 
 .controller('ChartController', function($scope, $state, Main, Chart) {
   
-  //
-  $scope.refreshChart = function(){
-    console.log('list: ', Main.userObject.stocks);
-    
+  $scope.chartData = [];
+  
+  // Calls /chart API to retrieve all stock data
+  $scope.refreshChart = function(){    
     Chart.updateData()
       .then(function(data){
-        console.log('chart data: ', data.data);
         // If successful login
         if (data.status === 200) {
-          
+          console.log('Successful retrieval: ', data);  
+          console.log('chart data: ', data.data);
+          // Store stock data inside chart data
+          $scope.chartData = data.data;
         } else {
           // Displays login error
-          
+            // TODO
         }
       })
       .catch(function(data){
