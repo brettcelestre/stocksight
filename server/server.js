c4c7189278720e9740389f6f70b1b3873186803b
Brett Celestre <bcelestre@gmail.com>
2016-10-13 18:57:34 -0700

    Server.js test for deployment

--- server/server.js
+++ server/server.js
@@ -1,32 +1,26 @@
+// main server file run in node or with nodemon to start server
 var server;
-    // Requires express
-var express = require('express'),
-    // Requires mongoose
-    mongoose = require('mongoose'),
-    // Imports middleware
-    middleware = require('./config/middleware.js'),
-    // Sets up Q promises
-    Q = require('q'),
-    // Requires express session
-    session = require('express-session'),
-    // Connects mongodb to sessions
-    MongoStore = require('connect-mongo')(session),
-    // Imports config file for connecting mongodb
-    // config = require('./config/config.js'),
-    // connection to mongodb
-    options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
-                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } } },
-    // express server instance
-    app = express();
-
-// Make mongoose use q promises
+var express = require('express');
+var middleware = require('./config/middleware.js');
+var mongoose = require('mongoose');
+var Q = require('q');
+// make mongoose use q promises
 mongoose.Promise = Q.Promise;
+var session = require('express-session');
+// Connects mongodb to sessions
+var MongoStore = require('connect-mongo')(session);
 
-// Connects to remote MongoDB
+// connection to mongodb
+var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } } };       
+ 
 var mongodbUri = process.env.mongoDB;
+ 
 mongoose.connect(mongodbUri, options);
-
 var conn = mongoose.connection;
+ 
+var app = express();
+
 conn.on('error', console.error.bind(console, 'connection error:'));
 
 // use mongo to store sessions
@@ -37,15 +31,17 @@ app.use(session({
   saveUninitialized: true
 }));
 
-//SET UP MIDDLEWARE + ROUTES
+// SET UP MIDDLEWARE + ROUTES
 middleware(app, express);
 
-var PORT = process.env.PORT || 9000;
+var PORT = process.env.PORT || 3000;
 
-// Save the HTTP server created with express as a variable in order to reuse for socket.io
 server = app.listen(PORT);
-console.log('Listening on', PORT); 
+console.log('qurvey is listening on ' + PORT);
 
-// export app
+// export for test
 module.exports = app;
+
+
+
 
