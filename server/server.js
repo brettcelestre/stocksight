b74214b35419ffa0863ac068f416261780c86067
Brett Celestre <bcelestre@gmail.com>
2016-10-13 18:17:53 -0700

    Updates MongoDB connection

--- server/server.js
+++ server/server.js
@@ -22,13 +22,9 @@ var express = require('express'),
 // Make mongoose use q promises
 mongoose.Promise = Q.Promise;
 
-// PROD: Connects to remote MongoDB
+// Connects to remote MongoDB
 var mongodbUri = process.env.mongoDB;
-
-mongoose.connect(mongodbUri, options);
-
-// DEV: Connects to local database
-mongoose.connect('mongodb://localhost/stocksight');
+mongoose.connect(mongodbUri, options);g
 
 var conn = mongoose.connection;
 conn.on('error', console.error.bind(console, 'connection error:'));
