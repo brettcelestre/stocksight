05a0e80e29bd3cc783fc685328fdf3d1c5c7c78d
Brett Celestre <bcelestre@gmail.com>
2016-10-14 21:37:00 -0700

    Adds loading gif on chart view

--- client/app/shared/chart/chartController.js
+++ client/app/shared/chart/chartController.js
@@ -3,16 +3,28 @@ angular.module('StockSight.main.chart', [])
 
 .controller('ChartController', function($scope, $state, Main, Chart) {
   
+  // Stores all stock data for chart  
   $scope.chartData = [];
-  
+
   // Calls /chart API to retrieve all stock data
-  $scope.refreshChart = function(){    
+  $scope.refreshChart = function(){  
+    // Selects Spinner and Chart DOM elements    
+    $spinner = document.getElementById('spinner');
+    $chart = document.getElementById('chart');
+    // Hides Chart
+    $chart.style.display = "none";
+    // Shows Spinner
+    $spinner.style.display = "block";
+    // Calls API for stock data
     Chart.updateData()
       .then(function(data){
-        // If successful login
+        // If successful request
         if (data.status === 200) {
-          console.log('Successful retrieval: ', data);  
-          console.log('chart data: ', data.data);
+          // console.log('Chart Stock Data: ', data.data);
+          // Hide Spinner
+          $spinner.style.display = "none";
+          // Show Chart
+          $chart.style.display = "block";
           // Store stock data inside chart data
           $scope.chartData = data.data;
         } else {
@@ -23,6 +35,11 @@ angular.module('StockSight.main.chart', [])
       .catch(function(data){
         console.error('Error with login: ', data);
       });
+  };
+  
+  // init function calls refreshChart on view load
+  $scope.init = function(){
+    $scope.refreshChart();
   }();
 
 });
