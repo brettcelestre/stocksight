9c0fe2129c0464c70d2b34f643575559194b9003
Brett Celestre <bcelestre@gmail.com>
2016-12-28 00:01:46 -0800

    Adds number filter to 2 decimal places in chartController

--- client/app/shared/chart/chartController.js
+++ client/app/shared/chart/chartController.js
@@ -46,4 +46,28 @@ angular.module('StockSight.main.chart', [])
     $scope.refreshChart();
   }();
 
-});
+})
+
+// Filters numbers to 2 decimal places
+.filter('numberEx', ['numberFilter', '$locale',
+  function(number, $locale) {
+
+    var formats = $locale.NUMBER_FORMATS;
+    return function(input, fractionSize) {
+      //Get formatted value
+      var formattedValue = number(input, fractionSize);
+
+      //get the decimalSepPosition
+      var decimalIdx = formattedValue.indexOf(formats.DECIMAL_SEP);
+
+      //If no decimal just return
+      if (decimalIdx == -1) return formattedValue;
+
+
+      var whole = formattedValue.substring(0, decimalIdx);
+      var decimal = (Number(formattedValue.substring(decimalIdx)) || "").toString();
+
+      return whole +  decimal.substring(1);
+    };
+  }
+]);
