d08677c32defdfdfac7757240b774b0ca6003cbe
Brett Celestre <bcelestre@gmail.com>
2016-12-29 11:21:33 -0800

    Updates chart service

--- client/app/shared/chart/chartService.js
+++ client/app/shared/chart/chartService.js
@@ -2,7 +2,7 @@
 angular.module('StockSight.main.chart.service', [])
 
 .service('Chart', function($http) {
-  
+    
   // Sends stock symbol to /stock
   var updateData = function() {
     return $http({
