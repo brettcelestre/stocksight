c26951dadb3e0fea75f6bb6af09771e65b9e70d3
Brett Celestre <bcelestre@gmail.com>
2017-03-01 20:48:01 -0800

    Inputs Angular components/services into module file

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -1,15 +1,15 @@
-// var loginservice = require('./components/login/loginService.js');
-// var signupservice = require('./components/signup/signupService.js');
-// var mainService = require('./components/main/mainService.js');
-// var homeController = require('./components/home/homeController.js');
-// var loginController = require('./components/login/loginController.js');
-// var signupController = require('./components/signup/signupController.js');
-// var mainController = require('./components/main/mainController.js');
-// var sidebarController = require('./shared/sidebar/sidebarController.js');
-// var sidebarService = require('./shared/sidebar/sidebarService.js');
-// var chartController = require('./shared/chart/chartController.js');
-// var chartService = require('./shared/chart/chartService.js');
-// var routes = require('./app.routes.js');
+var loginservice = require('./components/login/loginService.js');
+var signupservice = require('./components/signup/signupService.js');
+var mainService = require('./components/main/mainService.js');
+var homeController = require('./components/home/homeController.js');
+var loginController = require('./components/login/loginController.js');
+var signupController = require('./components/signup/signupController.js');
+var mainController = require('./components/main/mainController.js');
+var sidebarController = require('./shared/sidebar/sidebarController.js');
+var sidebarService = require('./shared/sidebar/sidebarService.js');
+var chartController = require('./shared/chart/chartController.js');
+var chartService = require('./shared/chart/chartService.js');
+var routes = require('./app.routes.js');
 
 angular.module('StockSight', [
   'ui.router',
