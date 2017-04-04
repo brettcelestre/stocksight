5f74455080fe698e69fec2019a6a2529731fd133
Brett Celestre <bcelestre@gmail.com>
2017-04-04 12:51:21 -0700

    Updates npm start build

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,8 @@
 
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
-    
+    sass = require('gulp-sass'),
+    webpack = require('gulp-webpack');
+
 gulp.task('css', function() {
   return gulp.src('./client/assets/sass/styles.scss')
       .pipe(sass().on('error', sass.logError))
@@ -12,4 +13,10 @@ gulp.task('watch', function() {
   gulp.watch(['./client/assets/sass/styles.scss', 'Gulpfile.js'], ['css']);
 });
 
-gulp.task('default', ['watch']);
+gulp.task('webpack', function() {
+  return gulp.src('.app.modules.js')
+  .pipe(webpack( require('./webpack.config') ))
+  .pipe(gulp.dest('client/'));
+});
+
+gulp.task('default', ['watch', 'webpack']);
